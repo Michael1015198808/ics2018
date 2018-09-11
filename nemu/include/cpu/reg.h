a380c943a1897a068d033cb4144c7a9cc907c4f1
tracer-ics2018 <tracer@njuics.org>
2018-09-11 01:38:08 +0000

    >  compile 171240518 Michael Linux 5c2020549004 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  01:38:08 up  1:29,  2 users,  load average: 0.01, 0.01, 0.00 68011ebad3b9dc456f8eee46b74f8ea5ce9d8d84

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,7 +14,8 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef union {
+typedef struct {
+  union{
   union {
     uint32_t _32;
     uint16_t _16;
@@ -27,7 +28,7 @@ typedef union {
    * in PA2 able to directly access these registers.
    */
   rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+  };
   vaddr_t eip;
 
 } CPU_state;
