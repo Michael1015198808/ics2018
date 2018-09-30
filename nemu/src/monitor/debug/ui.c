4aa0dc2a2a9c9be6bf72e51e0a0e9db3333b6634
tracer-ics2018 <tracer@njuics.org>
2018-09-30 09:35:30 +0000

    >  compile 171240518 Michael Linux 5c2020549004 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  09:35:30 up 15:29,  5 users,  load average: 0.00, 0.00, 0.00 3174d93dd45ed5edcfb3c3c67daccf4ce42a5a72

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -97,7 +97,9 @@ static int cmd_x(char *args){
 }
 static int cmd_w(char *args){
 	WP *temp=new_wp();
+	printf("%s\n",args);
 	strcmp(temp->exprr,args);
+	printf("%s\n",temp->exprr);
 	return 0;
 }
 static int cmd_d(char *args){
