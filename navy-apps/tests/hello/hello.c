3dcabf8cab6fde0e1784102dc45024bb1476ff04
tracer-ics2018 <tracer@njuics.org>
2018-12-14 00:02:28 +0800

    >  run try to Edit hello.c 171240518 michael 贪玩蓝月独家冠名Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  00:02:28 up 10:18, 11 users,  load average: 0.32, 0.25, 0.25 9e41bb1055ecaf7957a25a436930e05483e719f8

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -9,7 +9,7 @@ int main() {
     j ++;
     if (j == 10000) {
       //printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      printf("Hello%d\n",i++);
+      printf("Hello %d\n",i++);
       j = 0;
     }
   }
