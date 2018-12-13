1c7946512d78bade3fd7d30c1fcb05237e96c78b
tracer-ics2018 <tracer@njuics.org>
2018-12-13 21:12:40 +0800

    >  run try to check printf 171240518 michael 贪玩蓝月独家冠名Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  21:12:40 up  7:29, 11 users,  load average: 0.20, 0.33, 0.39 9fc884a32225e0ae4a33e4e56838710085b3f5e7

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -34,7 +34,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count){
   //_syscall_(SYS_write, fd, "****", 1);
-  _syscall_(SYS_write, fd, buf, count+2);
+  _syscall_(SYS_write, fd, buf, count);
   //while(1);
   return 0;
 }
