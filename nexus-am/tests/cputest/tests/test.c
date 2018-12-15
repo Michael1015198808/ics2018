77bba8b51d60a5e7958e6d9ef269b5b76abe9bc3
tracer-ics2018 <tracer@njuics.org>
2018-12-15 09:40:50 +0800

    >  run try to test instructions 171240518 michael 贪玩蓝月独家冠名Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  09:40:50 up 1 day, 19:57,  4 users,  load average: 0.33, 0.47, 0.48 d3011a70e319e0fabd38b2bf3104c4afd7611136

--- nexus-am/tests/cputest/tests/test.c
+++ nexus-am/tests/cputest/tests/test.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
+//
 void move(char x, char y,char z,int d) {
     if (d>0){
     move(x,z,y,d-1);
 	static int i=0;
     i=i+1;
-    printf("%d.%c to %c \n",i,x,z);
     move(y,z,x,d-1);
 }
 }
-main(){
+int main(){
 	char a='a';
 	char b='b';
 	char c='c';
 	move(a,b,c,5);
-	system("pause");
 }
