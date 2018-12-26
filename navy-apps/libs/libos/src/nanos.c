0df5ec5d4e8832a545a6f9ac8277860d59b94c7a
tracer-ics2018 <tracer@njuics.org>
2018-12-26 15:39:13 +0800

    >  run try to test set break 171240518 michael 贪玩蓝月独家冠名Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  15:39:13 up  3:47,  8 users,  load average: 0.26, 0.29, 0.23 b0a3487af6ee935d25019ce3af30037de98f213f

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -45,7 +45,7 @@ void *_sbrk(intptr_t increment){
   //write(1,num,strlen(num));
   p_break+=increment;
   int test=_syscall_(SYS_brk, (intptr_t)p_break,0,0);
-  sprintf(num,"%d\n",test);
+  sprintf(num,"return value:%d\n",test);
   write(1,num,strlen(num));
   //p_break=_syscall_(SYS_brk, p_break+increment,0,0)==0?p_break+increment:p_break;
   return old_break;
