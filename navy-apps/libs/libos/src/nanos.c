94ed8f533a3357cc9d09d588290106ef0b46a57e
tracer-ics2018 <tracer@njuics.org>
2018-12-26 15:38:59 +0800

    >  run try to test set break 171240518 michael 贪玩蓝月独家冠名Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  15:38:59 up  3:46,  8 users,  load average: 0.25, 0.29, 0.23 41581cbdc7e66f1b868f8a46fe96a3b3272ddd7b

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -37,11 +37,18 @@ int _write(int fd, void *buf, size_t count){
 }
 
 char num[40]="贪玩蓝月贪玩蓝月贪玩蓝月";
+extern int _end;
 void *_sbrk(intptr_t increment){
-  sprintf(num,"%d\n",increment);
+  static void* p_break=&_end;
+  void* old_break=p_break;
+  //sprintf(num,"%p\n",old_break);
+  //write(1,num,strlen(num));
+  p_break+=increment;
+  int test=_syscall_(SYS_brk, (intptr_t)p_break,0,0);
+  sprintf(num,"%d\n",test);
   write(1,num,strlen(num));
-  return _syscall_(SYS_brk, increment,0,0);
   //p_break=_syscall_(SYS_brk, p_break+increment,0,0)==0?p_break+increment:p_break;
+  return old_break;
   //return (void *)-1;
 }
 
