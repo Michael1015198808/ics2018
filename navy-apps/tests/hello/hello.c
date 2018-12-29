6af102a794705965f23f6ae375fb08439c0d0270
tracer-ics2018 <tracer@njuics.org>
2018-12-29 23:18:51 +0800

    >  run try to fix popa 171240518 michael 贪玩蓝月独家冠名Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  23:18:51 up  9:08, 15 users,  load average: 2.04, 1.46, 1.14 7a1349c821f7bcadfc31412ed96754f5bda9d20

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,10 +4,6 @@
 int main() {
   write(1, "Hello World!\n", 13);
   //printf("12345678\n");
-  printf("abcdefg\n");
-  printf("abcdefgh\n");
-  //printf("Hello World\n");
-  return 0;
   int i = 2;
   volatile int j = 0;
   while (1) {
