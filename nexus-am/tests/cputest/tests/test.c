d952e26537763c3637ab31639634f16aafcfcbce
tracer-ics2018 <tracer@njuics.org>
2018-12-29 15:21:03 +0800

    >  run try to empty(default) 171240518 michael 贪玩蓝月独家冠名Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  15:21:03 up  1:10,  8 users,  load average: 2.29, 2.31, 2.34 1776bf37c42b5c194b94e50453aaeb0e5edf2cc

--- nexus-am/tests/cputest/tests/test.c
+++ nexus-am/tests/cputest/tests/test.c
@@ -1,15 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-int Fibonacci(int x){
-if (x>3){
-   return Fibonacci(x-3)+2*Fibonacci(x-2);
-   }else return (x-1)/2+1;
-}
- 
-    int main(){
-    int i=25,t=25;
-    //while(scanf("%d%d",&i,&t)==2){
-    i=i*Fibonacci(t/5+1);
-    //printf("%d\n",i);}
+#include "trap.h"
+int main(){
+    printf("Hello from another side");
     return 0;
 }
