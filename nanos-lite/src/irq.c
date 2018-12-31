10ca39cffe334522066daa9888a3804eed24b822
tracer-ics2018 <tracer@njuics.org>
2018-12-31 10:19:16 +0800

    >  run try to test fs functions 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:19:16 up  1:52,  4 users,  load average: 1.62, 1.51, 1.47 427e3cda899ee4656a6ed7b47b5c5f9660474f95

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,9 +6,8 @@ static _Context *do_event(_Event e, _Context *c) {
             printf("一句话即可\n");
             break;
         case _EVENT_SYSCALL:
-            Log("brfore event%d,%d,%x,%d\n",c->GPR1,c->GPR2,c->GPR3,c->GPR4);
+            Log("event%d,%d,%x,%d\n",c->GPR1,c->GPR2,c->GPR3,c->GPR4);
             do_syscall(c);
-            Log(" after event%d,%d,%x,%d\n",c->GPR1,c->GPR2,c->GPR3,c->GPR4);
             break;
         default:
             panic("Unhandled event ID = %d", e.event);
