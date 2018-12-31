b8a1d0267f6be21f85dc71aff947bd71ff303137
tracer-ics2018 <tracer@njuics.org>
2018-12-31 10:31:39 +0800

    >  run try to test fs_read 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:31:39 up  2:04,  4 users,  load average: 1.06, 1.12, 1.18 619dcef340f9f49791cbe8566a90a668cf856f08

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -49,7 +49,7 @@ void *_sbrk(intptr_t increment){
 }
 
 int _read(int fd, void *buf, size_t count) {
-  return _syscall(SYS_read,fd,buf,count);
+  return _syscall_(SYS_read,fd,buf,count);
   return 0;
 }
 
