f035c6ec2c8d2e59e35aec185ee40b720125e15f
tracer-ics2018 <tracer@njuics.org>
2018-12-31 11:34:04 +0800

    >  run try to test fs_read 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:34:04 up  3:07,  5 users,  load average: 1.80, 1.30, 1.30 e350888497560086db787178b38c41a95729332

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -15,24 +15,24 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf("1\n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-  printf("1\n");
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
+  printf("1\n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
+  printf("1\n");
 
   fclose(fp);
 
