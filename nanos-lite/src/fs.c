dcbfb45fc9ee3085a3b5fcf663df6ec7674d33ac
tracer-ics2018 <tracer@njuics.org>
2018-12-31 10:04:29 +0800

    >  run try to test fs functions 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:04:29 up  1:37,  3 users,  load average: 1.14, 1.28, 1.40 21d588fc265ae6a76e41188ea61d77b654531fbc

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -39,9 +39,9 @@ void init_fs() {
 }
 int fs_open(const char *pathname, int flags, int mode){
     int i=0;
-    printf(pathname);
+    printf("%s\n",pathname);
     for(i=0;i<NR_FILES;++i){
-        printf(file_table[i].name);
+        printf("%s\n",file_table[i].name);
         if(!strcmp(file_table[i].name,pathname)){
             break;
         }
