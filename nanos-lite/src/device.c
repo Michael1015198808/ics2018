ac1399fc85954d9f09957f78f0766252544b3a61
tracer-ics2018 <tracer@njuics.org>
2019-01-01 01:31:29 +0800

    >  run try to test VGA 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  01:31:29 up  8:49,  1 user,  load average: 0.60, 0.50, 0.47 6e141af651c7edd736720abc47500cb1bd7f63c0

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,6 +28,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  printf("Write\n");
   int i, x, y, w = screen_width(), h = screen_height();
   y = offset / w;
   x = offset - y * w;
