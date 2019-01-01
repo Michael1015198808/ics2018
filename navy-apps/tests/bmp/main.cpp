c40686fd6ae10ac789100b0ce86740ab14be3730
tracer-ics2018 <tracer@njuics.org>
2019-01-01 11:36:09 +0800

    >  run try to test VGA 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:36:09 up  2:35,  9 users,  load average: 0.51, 1.00, 0.99 59998c2a3e76acbc776ccf5c94b525f59cd1d924

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -5,16 +5,20 @@
 
 int main() {
   //puts("Hello0");
-  NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
+  NDL_Bitmap *bmp = (NDL_Bitmap *) malloc(sizeof(NDL_Bitmap));
   //puts("Hello1");
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
   puts("Hello2");
   assert(bmp->pixels);
-  NDL_OpenDisplay(bmp->w, bmp->h);
   puts("Hello3");
+  NDL_OpenDisplay(bmp->w, bmp->h);
+  puts("Hello4");
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
+  puts("Hello5");
   NDL_Render();
+  puts("Hello6");
   NDL_CloseDisplay();
+  puts("Hello7");
   while (1);
   return 0;
 }
