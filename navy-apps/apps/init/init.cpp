e76246224512b55f87e274f4fd2e3107b78182d8
鄢振宇 <171240518@smail.nju.edu.cn>
2019-01-03 01:38:45 +0800

    FINISH pa3.3

--- navy-apps/apps/init/init.cpp
+++ navy-apps/apps/init/init.cpp
@@ -69,8 +69,6 @@ int main(int argc, char *argv[], char *envp[]) {
       !freopen("/dev/tty", "w", stdout) ||
       !freopen("/dev/tty", "w", stderr)) {
     // the OS does not meet the spec
-    printf("Open files failed\n");
-    while(1);
     exit(1);
   }
 
