33e9990602799f0f49481f4cade2a1ba5d72d53d
tracer-ics2018 <tracer@njuics.org>
2019-01-03 01:34:05 +0800

    >  run try to test batch 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  01:34:05 up 14:49,  4 users,  load average: 1.43, 0.80, 0.59 fde94b7017cf556baab1ba3af356b9894cc1bde2

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,8 +11,8 @@ _Context* do_syscall(_Context *c) {
 
   switch (a[0]) {
     case SYS_exit:
-      _halt(c->GPR2);
-      //naive_uload(NULL, "/bin/init");//Only for PA3.3
+      //_halt(c->GPR2);
+      naive_uload(NULL, "/bin/init");//Only for PA3.3
           printf("Should have exited\n");
           break;
     case SYS_yield:
