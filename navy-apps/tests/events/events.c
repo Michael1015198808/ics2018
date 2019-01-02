5749657e0f22dca088c14c36b863dd9f23398814
tracer-ics2018 <tracer@njuics.org>
2019-01-02 12:54:15 +0800

    >  run try to test events 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:54:15 up  2:09,  5 users,  load average: 1.92, 1.22, 0.72 a074162c298626363eb568902388a06a74b71a66

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
   FILE *fp = fopen("/dev/events", "r");
   volatile int j = 0;
   while(1) {
     j ++;
-    putchar('+');
+    if(rand()&1)putchar('+');
     if (j == 1000000) {
       char buf[256];
       char *p = buf, ch;
