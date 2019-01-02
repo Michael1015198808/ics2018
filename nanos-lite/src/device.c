ebff3c4df3424c838932c9c6ed5baf3243b3642e
tracer-ics2018 <tracer@njuics.org>
2019-01-02 12:49:26 +0800

    >  run try to test events 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:49:26 up  2:04,  5 users,  load average: 0.39, 0.57, 0.40 715bded6c4e2a0c1a243257e5ce92e194f1a02ed

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -56,7 +56,6 @@ void init_device() {
   Log("Initializing devices...");
   _ioe_init();
   sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d\n",width=screen_width(),height=screen_height());
-    printf("WIDTH:%d\nHEIGHT:%d\n",width,height);
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
