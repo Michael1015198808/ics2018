8125fd0d061a7a67256fc8bdf77ca4d2e4e83c0e
tracer-ics2018 <tracer@njuics.org>
2019-01-02 12:51:24 +0800

    >  run try to test events 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:51:24 up  2:06,  5 users,  load average: 1.40, 0.86, 0.53 325f895f68452e16a6e64d0c3375ac9b5e55de43

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,11 +21,10 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
     int key=read_key();
-    printf("read\n");
     if(key==0){
       return snprintf(buf,len, "t %d\n",0);
     }else{
-      return snprintf(buf,len, "k%c %s\n",key&0x8000?'U':'D',keyname[key&0x7fff]);
+      return snprintf(buf,len, "k%c %s\n",key&0x8000?'u':'d',keyname[key&0x7fff]);
     }
 }
 
