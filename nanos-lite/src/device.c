9fe4c656c3891514f8577b0091b288456c87df3a
tracer-ics2018 <tracer@njuics.org>
2019-01-04 15:07:11 +0800

    >  run try to speed up fb_write 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:07:11 up  6:00,  1 user,  load average: 1.05, 1.00, 1.01 9b74b5b19d639e8d37f326249bbc516cdc916d75

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -46,6 +46,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   for(i=0;i<len;++i){
       if(i+width-x>len)break;
     draw_rect((uint32_t*)buf,x,y,width-x,1);
+    buf+=width-x;
     i+=width-x;
     x=0;++y;
   }
