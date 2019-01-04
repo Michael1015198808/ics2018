fcceb71ad8cac9a20affcc90b391357efccbe8fb
tracer-ics2018 <tracer@njuics.org>
2019-01-04 15:12:02 +0800

    >  run try to speed up fb_write 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:12:02 up  6:05,  1 user,  load average: 1.87, 1.30, 1.11 f9615fa39467c59e232680ba2a3738bc3d1e7d92

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -39,6 +39,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  printf("%d\n",len);
   int i, x, y;
   offset>>=2;
   y = offset / width;
