6b749f9c58f1a08788a632eb41e247d3420c2ed8
tracer-ics2018 <tracer@njuics.org>
2019-01-22 18:06:41 +0800

    >  run try to test context switch 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:06:41 up  9:11,  6 users,  load average: 0.93, 0.71, 0.57 369458021efc4f055278a6e2648ef69f43c7c093

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,6 +33,7 @@ _Context* schedule(_Context *prev) {
     assert(pcb->cp->eip==(uintptr_t)hello_fun);
     current->cp=prev;
     current=&pcb[0];
+    Log("test");
     assert(pcb->cp->eip==(uintptr_t)hello_fun);
     return current->cp;
 }
