3c495489381ddc545e348f00e169db2512057197
tracer-ics2018 <tracer@njuics.org>
2019-01-23 22:55:08 +0800

    >  compile 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:55:08 up  4:16,  8 users,  load average: 0.66, 0.49, 0.41 436f66a8578e643b16a39af77699e9075896f7b2

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -64,7 +64,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 void init_device() {
   Log("Initializing devices...");
   _ioe_init();
-  sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d\n",width=screen_width(),height=screen_height());
+  sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d",width=screen_width(),height=screen_height());
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
