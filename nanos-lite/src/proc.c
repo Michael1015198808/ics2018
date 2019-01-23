eff52a54aff2d5be4b1465d55162c60c92090f06
tracer-ics2018 <tracer@njuics.org>
2019-01-23 17:36:29 +0800

    >  run try to multiprogramming 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:36:29 up  4:30, 11 users,  load average: 0.31, 0.36, 0.47 5b9878783fb8d1467ef06972800b95216e50022e

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,7 +29,6 @@ void init_proc() {
 _Context* schedule(_Context *prev) {
     Log("Schedule");
     current->cp=prev;
-    //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-    current=&pcb[1];
+    current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
     return current->cp;
 }
