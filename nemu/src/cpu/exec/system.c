5949587c03a78f7445d9069742eea8b18aae44db
tracer-ics2018 <tracer@njuics.org>
2019-01-28 23:03:11 +0800

    >  compile 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:03:11 up 2 days, 14:02, 13 users,  load average: 0.17, 0.27, 0.33 a8a1a50e904553ea3fdd48683183b6326759217

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -92,7 +92,7 @@ make_EHelper(out) {
 #include <stdarg.h>
 make_EHelper(cheat){
     if(paddr_read(cpu.esp+4,4)==1){
-        //printf((void*)(uintptr_t)paddr_read(cpu.esp+8,4),paddr_read(cpu.esp+12,4));
+        printf("'%s'",(char*)(uintptr_t)paddr_read(cpu.esp+8,4));//,paddr_read(cpu.esp+12,4));
     }else{
         printf("%d\n",paddr_read(cpu.esp+4,4));
     }
