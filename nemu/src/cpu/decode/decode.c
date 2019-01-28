37d22349f8709aba5c33d768c4c513ffcd6c807e
tracer-ics2018 <tracer@njuics.org>
2019-01-28 17:44:18 +0800

    >  compile 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:44:18 up 2 days,  8:43, 13 users,  load average: 0.44, 0.47, 1.02 f128f8602b03eaf72d057da7d11caec5182180ca

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -330,7 +330,7 @@ make_DHelper(out_a2dx) {
 void operand_write(Operand *op, rtlreg_t* src) {
   if (op->type == OP_TYPE_REG) { rtl_sr(op->reg, src, op->width); }
   else if (op->type == OP_TYPE_MEM) { rtl_sm(&op->addr, src, op->width); }
-  else if (op->type == OP_TYPE_CREG) { cpu.cr[op->reg]=(*src)&((1<<(op->width<<3))-1); }
+  else if (op->type == OP_TYPE_CREG) { cpu.cr[op->reg]=(*src);if(op->width!=4)assert(0); }
   else { assert(0); }
 }
 
