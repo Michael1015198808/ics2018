a4b3c6808055d77ac44a8403af29cb9c3503a684
tracer-ics2018 <tracer@njuics.org>
2019-01-28 17:55:27 +0800

    >  compile 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:55:27 up 2 days,  8:54, 13 users,  load average: 0.17, 0.35, 0.68 30840f1cfed2ca9dfdbcf2035b0e07f3f5763fe0

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -41,6 +41,7 @@ static inline paddr_t page_translate(vaddr_t va){
   Log("translate");
   Log("%x",va);
   Log("%x",cpu.CR3);
+  Log("%x",pde_idx);
   Log("%x",pde[pde_idx]);
   Log("%x",pte[pte_idx]);
   paddr_t pa=pte[pte_idx]+(va&(pow2(12)-1));
