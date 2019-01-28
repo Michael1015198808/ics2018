d309b92766c41c0b08d4246975fd6fa88ddc2df6
tracer-ics2018 <tracer@njuics.org>
2019-01-28 22:58:52 +0800

    >  compile 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:58:52 up 2 days, 13:57, 13 users,  load average: 0.32, 0.37, 0.36 60da26fff4bb717e7116e3a4efd857052ecdaab3

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -91,9 +91,10 @@ make_EHelper(out) {
 }
 #include <stdarg.h>
 make_EHelper(cheat){
-    int i;
-    for(i=0;i<10;++i){
-        printf("%d\n",paddr_read(cpu.esp+4*i,4));
+    if(paddr_read(cpu.esp+4,4)==1){
+        printf((void*)(uintptr_t)paddr_read(cpu.esp+8,4),paddr_read(cpu.esp+12,4));
+    }else{
+        printf("%d\n",paddr_read(cpu.esp+4,4));
     }
     Log("Cheat code!");
 }
