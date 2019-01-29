87406eabd65a30bae28435f312161973a372e9a0
tracer-ics2018 <tracer@njuics.org>
2019-01-29 17:01:51 +0800

    >  run test paging 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:01:51 up 3 days,  8:00, 14 users,  load average: 0.37, 0.28, 0.27 bc6e0e37ac53b963b8244092d313f5edcff1d2a8

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -48,6 +48,7 @@ int _vme_init(void* (*pgalloc_f)(size_t), void (*pgfree_f)(void*)) {
   set_cr3(kpdirs);
   test((uint32_t)kpdirs);
   get_cr0();
+  test((uint32_t)kpdirs);
   set_cr0(get_cr0() | CR0_PG);
   test((uint32_t)kpdirs);
 
