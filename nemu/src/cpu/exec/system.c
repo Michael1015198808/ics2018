d554c57a809bf605608a02a87a2aee787d97a6ab
tracer-ics2018 <tracer@njuics.org>
2019-01-29 17:04:38 +0800

    >  compile 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:04:38 up 3 days,  8:03, 14 users,  load average: 0.23, 0.29, 0.28 8b5753591b732b4b3d5211356dc454ede107ee9e

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -12,12 +12,14 @@ make_EHelper(lidt) {
 
 make_EHelper(mov_r2cr) {
   operand_write(id_dest, &id_src->val);
+    Log("%x,%x\n",cpu.CR0,cpu.CR3);
 
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
   operand_write(id_dest, &id_src->val);
+    Log("%x,%x\n",cpu.CR0,cpu.CR3);
 
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
