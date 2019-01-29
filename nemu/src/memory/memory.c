179b1493d1cb4b56972e98bd6d5cc2d1cc156a37
tracer-ics2018 <tracer@njuics.org>
2019-01-29 19:00:39 +0800

    >  compile 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:00:39 up 3 days,  9:59, 14 users,  load average: 0.25, 0.25, 0.25 b5ca2a2484188b9f85ed228592dc540e15a6f1c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -31,31 +31,66 @@ void paddr_write(paddr_t addr, uint32_t data, int len) {
 	}
 }
 #define pow2(_num) (1<<(_num))
-#define addr_join(_A,_B) \
-    (( \
-      ((uint32_t)_A)& \
-      (-pow2(12))) \
-      +(_B<<2))
-#define pde (((uintptr_t)cpu.CR3&-pow2(12)))
-#define pde_tab addr_join(pde,va.dir)
-#define pte (paddr_read(pde_tab,4))
-#define pte_tab addr_join(pte,va.page)
-#define pa ((paddr_read(pte_tab,4)&~0xfff)+va.offset)
-static inline paddr_t page_translate(vaddr_t addr){
-  union{
-      struct{
-          uint32_t offset:12;
-          uint32_t page:10;
-          uint32_t dir:10;
-      };
-      uint32_t val;
+#define declare_va \
+union{ \
+   struct{ \
+     uint32_t offset:12; \
+     uint32_t page:10; \
+     uint32_t dir:10; \
+   }; \
+   uint32_t val; \
   } va;
+#define declare_pde \
+  union{ \
+    struct { \
+      uint32_t present             : 1; \
+      uint32_t read_write          : 1; \
+      uint32_t user_supervisor     : 1; \
+      uint32_t page_write_through  : 1; \
+      uint32_t page_cache_disable  : 1; \
+      uint32_t accessed            : 1; \
+      uint32_t pad0                : 6; \
+      uint32_t page_frame          : 20; \
+    }; \
+    uint32_t val; \
+  } PDE;
+#define declare_pte \
+  union{ \
+    struct { \
+      uint32_t present             : 1; \
+      uint32_t read_write          : 1; \
+      uint32_t user_supervisor     : 1; \
+      uint32_t page_write_through  : 1; \
+      uint32_t page_cache_disable  : 1; \
+      uint32_t accessed            : 1; \
+      uint32_t dirty               : 1; \
+      uint32_t pad0                : 1; \
+      uint32_t global              : 1; \
+      uint32_t pad1                : 3; \
+      uint32_t page_frame          : 20; \
+    }; \
+    uint32_t val; \
+  } PTE;
+#define join_read(_A,_B) paddr_read(addr_join(_A,_B),4)
+#define addr_join(_A,_B) \
+    (((_A)<<(32-20)) \
+      +(_B))
+static inline paddr_t page_translate(vaddr_t addr){
+  declare_va;declare_pde;declare_pte;
+
   va.val=addr;
-  Log("va %x",addr);
-  Log("pde%lx,%x",pde,pde_tab);
-  Log("pte%x,%x",pte,pte_tab);
-  Log("pa %x",pa);
-  *(int*)0=0;
+
+  PDE.val=
+      join_read(cpu.CR3>>12,va.dir);
+  assert(PDE.present);
+
+  PTE.val=
+      join_read(PDE.page_frame,va.page);
+  assert(PTE.present);
+
+  paddr_t pa=join_read(PTE.page_frame,va.offset);
+  assert(addr==pa);//temp test
+ *(int*)0=0;
   return pa;
 }
 #define CROSS_PAGE (((addr+len-1)&pow2(12))<len-1)
