514acfee28e4dc21140dd45babff9ba040b61d15
tracer-ics2018 <tracer@njuics.org>
2019-02-02 23:05:32 +0700

    >  run test empty(default) 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:05:32 up 3 days,  1:33,  8 users,  load average: 0.20, 0.20, 0.16 cf2b1f5943de2b57d6f005afb3c0eb0054716cba

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,9 +3,9 @@ _Context* do_syscall(_Context*);
 static _Context *do_event(_Event e, _Context *c) {
     switch (e.event) {
         case _EVENT_YIELD:
-            //printf("一句话即可\n");
+            printf("一句话即可\n");
             //Set in PA3.1
-            return schedule(c);
+            //return schedule(c);
             break;
         case _EVENT_SYSCALL:
             //Log("syscall%d,%d,%x,%d",c->GPR1,c->GPR2,c->GPR3,c->GPR4);
