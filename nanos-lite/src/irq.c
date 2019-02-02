45f100f4bb3bdc73d54b6b634d8341c111b912c9
tracer-ics2018 <tracer@njuics.org>
2019-02-02 23:15:33 +0700

    >  run test empty(default) 171240518 michael Linux neco 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:15:33 up 3 days,  1:43,  8 users,  load average: 0.37, 0.35, 0.28 86e3d4c5ff894a5bceedec5a6493a2d34e3c478f

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ static _Context *do_event(_Event e, _Context *c) {
         case _EVENT_YIELD:
             printf("一句话即可\n");
             //Set in PA3.1
-            //return schedule(c);
+            return schedule(c);
             break;
         case _EVENT_SYSCALL:
             //Log("syscall%d,%d,%x,%d",c->GPR1,c->GPR2,c->GPR3,c->GPR4);
