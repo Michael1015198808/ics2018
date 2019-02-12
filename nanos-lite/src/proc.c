3faedb33a538ec76a2be48c80c3e0ed60be4cabb
tracer-ics2018 <tracer@njuics.org>
2019-02-12 19:14:34 +0800

    >  run test program switch 171240518 michael Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  19:14:34 up 1 day,  7:43, 10 users,  load average: 0.63, 0.73, 0.61 8db17017ee26e3a24adde204ab4c0039d19baa99

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,14 +22,16 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   //context_kload(&pcb[1],(void*)hello_fun);
-  context_uload(&pcb[0], "/bin/pal");
-  context_uload(&pcb[1], "/bin/hello");
+  context_uload(&pcb[0], "/bin/hello");
+  context_uload(&pcb[1], "/bin/pal");
+  context_uload(&pcb[2], "/bin/dummy");
+  context_uload(&pcb[3], "/bin/init");
   switch_boot_pcb();
 }
 
 _Context* schedule(_Context *prev) {
     current->cp=prev;
     static uint8_t cnt=0;
-    current = (++cnt==0 ? &pcb[1] : &pcb[0]);
+    current = (++cnt==0 ? &pcb[0] : &pcb[1]);
     return current->cp;
 }
